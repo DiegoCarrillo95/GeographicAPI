7ae84e208f5de073f3423fe30fe06379a0f1f09b
Diego Carrillo <d.carrillo@cinq.com.br>
2018-08-06 11:20:25 -0300

    Adds state transformations

--- src/main/java/com/diego/geographicapi/util/Transformer.java
+++ src/main/java/com/diego/geographicapi/util/Transformer.java
@@ -1,7 +1,9 @@
 package com.diego.geographicapi.util;
 
 import com.diego.geographicapi.dto.CountryDto;
+import com.diego.geographicapi.dto.StateDto;
 import com.diego.geographicapi.model.Country;
+import com.diego.geographicapi.model.State;
 
 public class Transformer {
 	public static CountryDto countryModelToDtoTransformer(Country countryModel) {
@@ -21,4 +23,23 @@ public class Transformer {
 		//TODO: countryModel.setStates(states);
 		return countryModel;
 	}
+	
+	public static StateDto stateModelToDtoTransformer(State stateModel) {
+		StateDto stateDto = new StateDto();
+		stateDto.setId(stateModel.getId());
+		stateDto.setName(stateModel.getName());
+		stateDto.setStateCode(stateModel.getStateCode());
+		//TODO: stateDto.setCities(stateModel.getCities());
+		return stateDto;
+	}
+	
+	public static State stateDtoToModelTransformer(StateDto stateDto) {
+		State stateModel = new State();
+		stateModel.setId(stateDto.getId());
+		stateModel.setName(stateDto.getName());
+		stateModel.setStateCode(stateDto.getStateCode());
+		//TODO: stateModel.setCities(stateDto.getCities());
+		return stateModel;
+	}
+
 }
