95253ec1bf5cb01f245ca66accfc19a1eff048b7
Diego Carrillo <d.carrillo@cinq.com.br>
2018-08-13 13:49:47 -0300

    Adds integration tests class

--- src/integTest/java/com/diego/geographicapi/IntegrationTests.java
+++ src/integTest/java/com/diego/geographicapi/IntegrationTests.java
@@ -1,26 +1,71 @@
 package com.diego.geographicapi;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.diego.geographicapi.controller.CountryController;
+import com.diego.geographicapi.dto.CountryDto;
+import com.diego.geographicapi.model.Country;
+import com.diego.geographicapi.repository.CountryRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class IntegrationTests {
+	
+	@Autowired
+	private CountryController countryController; 
+	
+	@Autowired
+	private CountryRepository countryRepository;
+	
+	private final String countryName = "Brazil";
+	private final String countryCode = "BR";
 
+	private CountryDto countryDto;
+	private Country countryModel;
+	
+	
 	@Before
 	public void setup() {
-		MockitoAnnotations.initMocks(this);
+		countryDto = new CountryDto();
+		countryDto.setName(countryName);
+		countryDto.setCountryCode(countryCode);
+		
+		countryModel = new Country();
+		countryModel.setName(countryName);
+		countryModel.setCountryCode(countryCode);
+	}
+	
+	@After
+	public void teardown() {
+		countryRepository.deleteAll();
 	}
 
 	@Test
-	public void IntegrationTestExample() {
-
+	public void shouldAddCountryInDatabaseWhenInsertMethodIsCalledFromController() {		
+		countryDto = countryController.insertCountry(countryDto);
+		
+		assertEquals(countryName, countryRepository.findByCountryCode(countryCode).get().getName());
+		assertNotNull(countryRepository.findByCountryCode(countryCode).get().getId());
+	}
+	
+	@Test
+	public void shouldReturnCountryWhenGetCountryMethodIsCalledFromController() {
+		countryRepository.save(countryModel);
+		
+		CountryDto returnedCountryDto = countryController.getCountry(countryCode);
+		
+		assertEquals(countryDto, returnedCountryDto);
 	}
 
 }
